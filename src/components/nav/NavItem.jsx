da44904735bd6dee21ef8509901aabbfb4455736
Amit Raj <rajamitnine@gmail.com>
2025-12-16 15:59:20 +0530

    Apply conditional styling and onClick attribute to the rendered element

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -1,10 +1,11 @@
-export default function NavItem({ label, isLast }) {
+export default function NavItem({ label, isLast, isActive, onClickItem }) {
   return (
     <>
       <h2
-        className={`w-[20%] py-3 border-2 border-t-0 border-b-0 border-l-0 text-center ${
+        className={`w-[20%] py-3 cursor-pointer border-2 border-t-0 border-b-0 border-l-0 text-center ${
           isLast ? "border-r-0" : ""
-        }`}
+        } ${isActive ? "bg-red-500 text-[#fff]" : ""}`}
+        onClick={onClickItem}
       >
         {label}
       </h2>
