d56dc26836fffc415f23bae9e1a240e8ad6011e1
Amit Raj <rajamitnine@gmail.com>
2025-12-16 17:54:34 +0530

    Import and use Info and Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import Card from "./components/card/Card";
 import Header from "./components/header/Header";
+import Info from "./components/info/Info";
 import Nav from "./components/nav/Nav";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <div className="w-[1200px] mx-auto p-5">
         <Header />
         <Nav />
+        <Info />
+        <Card />
       </div>
     </>
   );
