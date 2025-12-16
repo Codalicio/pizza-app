416b5e56e97de8a2023030857cba01b91f765c35
Amit Raj <rajamitnine@gmail.com>
2025-12-16 19:57:26 +0530

    Accept sme props and use them

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Card from "./components/card/Card";
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Info from "./components/info/Info";
 import Nav from "./components/nav/Nav";
 
 function App() {
+  const [isActive, setIsActive] = useState("Pizzas");
   return (
     <>
-      <div className="w-[1200px] mx-auto p-5">
+      <div className="w-[1200px] mx-auto p-5 box-border">
         <Header />
-        <Nav />
-        <Info />
-        <Card />
+        <Nav activeTab={isActive} setActiveTab={setIsActive} />
+        <Info activeTab={isActive} />
       </div>
     </>
   );
