c93c84b2d470eced0bc2a45d685f9f2b2342b79f
Amit Raj <rajamitnine@gmail.com>
2025-12-16 15:58:36 +0530

    Use useState hook, define isActive and onClickItem props

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import NavItem from "./NavItem";
 
 export default function Nav() {
   const navItems = ["Pizzas", "Sides", "Desserts", "Drinks", "Deals"];
+
+  const [isActive, setIsActive] = useState("Pizzas");
+
   return (
     <>
       <div className="flex my-3 border-2">
@@ -10,6 +14,8 @@ export default function Nav() {
             key={item}
             label={item}
             isLast={index === navItems.length - 1}
+            isActive={isActive === item}
+            onClickItem={() => setIsActive(item)}
           />
         ))}
       </div>
