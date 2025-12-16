ea3d0229de47f0d73c31e587e4dc2af4282a7019
Amit Raj <rajamitnine@gmail.com>
2025-12-16 20:24:36 +0530

    Add a class to the element

--- src/components/nav/NavItem.jsx
+++ src/components/nav/NavItem.jsx
@@ -2,7 +2,7 @@ export default function NavItem({ label, isLast, isActive, onClickItem }) {
   return (
     <>
       <h2
-        className={`w-[20%] py-3 cursor-pointer border-2 border-t-0 border-b-0 border-l-0 text-center ${
+        className={`w-[20%] py-3 cursor-pointer border-2 border-gray-300 border-t-0 border-b-0 border-l-0 text-center ${
           isLast ? "border-r-0" : ""
         } ${isActive ? "bg-red-500 text-[#fff]" : ""}`}
         onClick={onClickItem}
