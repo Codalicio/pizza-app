28157b8d239ed3f461031429d09a381bb1e3f78e
Amit Raj <rajamitnine@gmail.com>
2025-12-16 17:53:59 +0530

    Add a info section with title and description

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,13 @@
+export default function Info() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-center my-3">Pizzas</h1>
+        <p className="text-md text-center mb-15">
+          Explore our great range of pizza recipes from the Pizza Hut pizza menu
+          .
+        </p>
+      </div>
+    </>
+  );
+}
