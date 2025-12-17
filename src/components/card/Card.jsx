c2bd51b0a50785ab817e1cd622fc4c8b42154917
Amit Raj <rajamitnine@gmail.com>
2025-12-17 16:42:50 +0530

    Add some responsive tailwindcss styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,11 @@
 export default function Card({ image, title, description }) {
   return (
     <>
-      <div className="w-[25%] border-2 border-gray-300 px-3">
-        <img src={image} className="w-full my-3" />
-        <h2 className="my-3 font-bold text-md">{title}</h2>
-        <p className="my-3 text-sm">{description}</p>
-        <button className="w-full bg-green-700 py-3 rounded-md my-3 text-[#ffffff] cursor-pointer">
+      <div className="border-2 border-gray-300 p-4 rounded-md">
+        <img src={image} className="w-full my-3 object-cover" />
+        <h2 className="my-2 font-bold text-sm sm:text-md">{title}</h2>
+        <p className="my-2 text-xs sm:text-sm">{description}</p>
+        <button className="w-full bg-green-700 py-2 rounded-md my-3 text-[#ffffff] cursor-pointer">
           View More
         </button>
       </div>
