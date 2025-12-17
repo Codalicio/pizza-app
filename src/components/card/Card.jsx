fe748bfd01a3476bb5af7c191776f365a540ffcb
Amit Raj <rajamitnine@gmail.com>
2025-12-17 20:38:15 +0530

    Add some tailwindcss styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,13 @@
 export default function Card({ image, title, description }) {
   return (
     <>
-      <div className="border-2 border-gray-300 p-4 rounded-md">
+      <div className="border-2 border-gray-300 p-4 rounded-md flex flex-col items-center">
         <img src={image} className="w-full my-3 object-cover" />
-        <h2 className="my-2 font-bold text-sm sm:text-md">{title}</h2>
-        <p className="my-2 text-xs sm:text-sm">{description}</p>
-        <button className="w-full bg-green-700 py-2 rounded-md my-3 text-[#ffffff] cursor-pointer">
+        <h2 className="my-2 font-bold text-sm sm:text-md text-center">
+          {title}
+        </h2>
+        <p className="my-2 text-xs sm:text-sm text-center">{description}</p>
+        <button className="mt-auto w-full bg-green-700 py-2 sm:py-3 rounded-md my-3 text-[#ffffff] cursor-pointer">
           View More
         </button>
       </div>
