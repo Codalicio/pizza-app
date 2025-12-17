dbb9e5e6eb862843750f36008773002fc0075502
Amit Raj <rajamitnine@gmail.com>
2025-12-17 13:22:03 +0530

    Add a tailwindcss class

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
-      <div className="w-[1200px] mx-auto p-5 box-border">
+      <div className="w-300 mx-auto p-5 box-border">
         <Header />
         <Nav activeTab={isActive} setActiveTab={setIsActive} />
         <Search
