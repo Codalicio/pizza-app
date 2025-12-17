0ec799defd242c98c8027801c1e855ef673403ff
Amit Raj <rajamitnine@gmail.com>
2025-12-17 10:46:36 +0530

    Some change

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,15 @@
+export default function Search({ activeTab, searchTerm, setSearchTerm }) {
+  return (
+    <>
+      <div className="w-[1200px] my-10 flex justify-center">
+        <input
+          type="text"
+          className="border-2 border-gray-300 w-[70%] p-2 rounded-md outline-gray-400"
+          placeholder={`Search ${activeTab}`}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </>
+  );
+}
