be0b1a8c8eda44e746e89ce8870ed87bf1a7b398
Amit Raj <rajamitnine@gmail.com>
2025-12-17 13:27:23 +0530

    Add a tailwindcss class

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,10 +2,10 @@ export default function Card({ image, title, description }) {
   return (
     <>
       <div className="w-[25%] border-2 border-gray-300 px-3">
-        <img src={image} className="w-[100%] my-3" />
+        <img src={image} className="w-full my-3" />
         <h2 className="my-3 font-bold text-md">{title}</h2>
         <p className="my-3 text-sm">{description}</p>
-        <button className="w-[100%] bg-green-700 py-3 rounded-md my-3 text-[#ffffff] cursor-pointer">
+        <button className="w-full bg-green-700 py-3 rounded-md my-3 text-[#ffffff] cursor-pointer">
           View More
         </button>
       </div>
