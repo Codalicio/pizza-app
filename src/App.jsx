4da817f17550fd4717386a063425b1b100044fb7
Amit Raj <rajamitnine@gmail.com>
2025-12-17 10:45:10 +0530

    Add a component and pass required props

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import Header from "./components/header/Header";
 import Info from "./components/info/Info";
 import Nav from "./components/nav/Nav";
+import Search from "./components/search/Search";
 
 function App() {
   const [isActive, setIsActive] = useState("Pizzas");
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
       <div className="w-[1200px] mx-auto p-5 box-border">
         <Header />
         <Nav activeTab={isActive} setActiveTab={setIsActive} />
-        <Info activeTab={isActive} />
+        <Search
+          activeTab={isActive}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+        <Info activeTab={isActive} searchTerm={searchTerm} />
       </div>
     </>
   );
