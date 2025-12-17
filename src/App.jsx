a100870c977fb8d67e983af76644e19abb68c096
Amit Raj <rajamitnine@gmail.com>
2025-12-17 16:22:34 +0530

    Add a responsive class

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
-      <div className="w-300 mx-auto p-5 box-border">
+      <div className="max-w-300 mx-auto p-4 box-border sm:p-6">
         <Header />
         <Nav activeTab={isActive} setActiveTab={setIsActive} />
         <Search
